1bb6e45beecf77596f556518cfcffe55e08f44ce
mahmoudmalhayek <mahmoudmm.alhayek@gmail.com>
2022-10-30 12:59:29 +0200

    new modification30

--- favoriteBook.js
+++ favoriteBook.js
@@ -1,10 +1,75 @@
-var retrievedData = localStorage.getItem("myBooks");
+const container = document.createElement("div");
+const navbar = document.createElement("div");
+document.body.append(navbar);
+document.body.append(container);
+container.setAttribute("class", "container");
+navbar.setAttribute("class", "navbar");
 
-var favBooks = JSON.parse(retrievedData);
-console.log(favBooks)
+var img = document.createElement("IMG");
+img.src = "https://www.iconpacks.net/icons/2/free-book-icon-4986-thumb.png";
+navbar.append(img);
+
+var ul = document.createElement("ul");
+navbar.appendChild(ul);
+var li = document.createElement("li");
+li.className = "file";
+var a = document.createElement("a");
+var a1 = document.createElement("a");
+var a2 = document.createElement("a");
+
+a.setAttribute("href", "http://127.0.0.1:5500/book-directory/index.html#");
+a1.setAttribute("href","http://127.0.0.1:5500/book-directory/favoriteBook.html");
+a.setAttribute("target", "blank");
+a1.setAttribute("target", "blank");
+a2.setAttribute("href", "#");
+a.innerHTML = "Home";
+a1.innerHTML = "FavoriteList";
+a2.innerHTML = "Settings";
+li.appendChild(a);
+li.appendChild(a1);
+li.appendChild(a2);
+ul.appendChild(li);
 
 
-let booksDisplayTitle = document.createTextNode("Title:\t"+favBooks[0].favorite + "\t");
-document.body.append(booksDisplayTitle);
+let retrievedData = localStorage.getItem("myBooks");
+let favBooks = JSON.parse(retrievedData);
 
-// document.getElementById("demo").innerHTML = favBooks
+favBooks.forEach((book, _index) => {
+  if (favBooks[_index].favorite == true) {
+    let booksDisplay = document.createElement("div");
+    booksDisplay.setAttribute("class", "booksDisplay");
+    container.appendChild(booksDisplay);
+
+    var img = document.createElement("IMG");
+    img.src = book.image;
+    booksDisplay.appendChild(img);
+
+    let booksDisplayTitle = document.createTextNode(
+      "Title:\t" + book.title + "\t"
+    );
+    booksDisplay.appendChild(booksDisplayTitle);
+
+    let booksDisplayAuthor = document.createTextNode(
+      "Author:" + book.author + "\t"
+    );
+    booksDisplay.appendChild(booksDisplayAuthor);
+
+    let booksDisplayEdition = document.createTextNode(
+      "Edition:" + book.edition
+    );
+
+    booksDisplay.appendChild(booksDisplayEdition);
+
+    const button = document.createElement("button");
+    button.setAttribute("id", `button${_index}`);
+    booksDisplay.appendChild(button);
+    button.innerText = "Remove from favorite";
+
+    button.addEventListener("click", favoriteButton);
+
+    function favoriteButton() {
+      favBooks[_index].favorite = false;
+      localStorage.setItem("favBooks", JSON.stringify([...favBooks]));
+    }
+  }
+});
--- script.js
+++ script.js
@@ -1,102 +1,112 @@
 const container = document.createElement("div");
-const navbar = document.createElement('div')
+const navbar = document.createElement("div");
 document.body.append(navbar);
 document.body.append(container);
-container.setAttribute('class','container')
-navbar.setAttribute('class','navbar')
+container.setAttribute("class", "container");
+navbar.setAttribute("class", "navbar");
 
 var img = document.createElement("IMG");
-img.src = 'https://www.iconpacks.net/icons/2/free-book-icon-4986-thumb.png';
+img.src = "https://www.iconpacks.net/icons/2/free-book-icon-4986-thumb.png";
 navbar.append(img);
 
 var ul = document.createElement("ul");
 navbar.appendChild(ul);
+var li = document.createElement("li");
+li.className = "file";
+var a = document.createElement("a");
+var a1 = document.createElement("a");
+var a2 = document.createElement("a");
 
-for (var i = 0; i <= 2; i++)
-{
-    const list = ["Home","Favorite List","Settings"]
-    var li = document.createElement("li");  
-    li.className = "file";
-    var a = document.createElement("a");
-    a.setAttribute('href','#');
-    a.innerHTML = list[i];
-    li.appendChild(a);
-    ul.appendChild(li);
+a.setAttribute("href", "http://127.0.0.1:5500/book-directory/index.html#");
+a1.setAttribute(
+  "href",
+  "http://127.0.0.1:5500/book-directory/favoriteBook.html"
+);
+a1.setAttribute("target", "blank");
+a2.setAttribute("href", "#");
+a.innerHTML = "Home";
+a1.innerHTML = "FavoriteList";
+a2.innerHTML = "Settings";
+li.appendChild(a);
+li.appendChild(a1);
+li.appendChild(a2);
+ul.appendChild(li);
 
-}
 let books = [
-    {
-        id: 1,
-        title: "land of zicola",
-        author: "Amro Abdelhamid",
-        edition: 2,
-        image:"https://mofibo.com/images/640x640/0000503069.jpg",
-        favorite : false
-    },
-    {
-        id: 2,
-        title: "Amarita",
-        author: "Amro Abdelhamid",
-        edition: 1,
-        image:"https://www.storytel.com/images/640x640/0000503067.jpg",
-        favorite: false
-    },
-    {
-        id: 3,
-        title: "Garteen",
-        author: "Amro Abdelhamid",
-        edition: 3,
-        image:"https://alfagrbookstore.com/image/catalog/products/347/New%20Doc%202021-05-29%2009.37.35_13.jpg",
-        favorite: false
-    },
-]
+  {
+    id: 1,
+    title: "land of zicola",
+    author: "Amro Abdelhamid",
+    edition: 1,
+    image: "https://mofibo.com/images/640x640/0000503069.jpg",
+    favorite: false,
+  },
+  {
+    id: 2,
+    title: "Amarita",
+    author: "Amro Abdelhamid",
+    edition: 3,
+    image: "https://www.storytel.com/images/640x640/0000503067.jpg",
+    favorite: false,
+  },
+  {
+    id: 3,
+    title: "Garteen",
+    author: "Amro Abdelhamid",
+    edition: 2,
+    image:
+      "https://alfagrbookstore.com/image/catalog/products/347/New%20Doc%202021-05-29%2009.37.35_13.jpg",
+    favorite: false,
+  },
+];
 
 let fav = [];
-books.forEach((book,_index)=> {
-    let booksDisplay = document.createElement('div');
-    booksDisplay.setAttribute('class', 'booksDisplay');
-    container.appendChild(booksDisplay);
+books.forEach((book, _index) => {
+  let booksDisplay = document.createElement("div");
+  booksDisplay.setAttribute("class", "booksDisplay");
+  container.appendChild(booksDisplay);
 
+  var img = document.createElement("IMG");
+  img.src = book.image;
+  booksDisplay.appendChild(img);
 
-    var img = document.createElement("IMG");
-    img.src = book.image;
-    booksDisplay.appendChild(img);
+  let booksDisplayTitle = document.createTextNode(
+    "Title:\t" + book.title + "\t"
+  );
+  booksDisplay.appendChild(booksDisplayTitle);
 
-    let booksDisplayTitle = document.createTextNode("Title:\t"+book.title + "\t");
-    booksDisplay.appendChild(booksDisplayTitle);
- 
-    let booksDisplayAuthor = document.createTextNode("Author:"+book.author + "\t");
-    booksDisplay.appendChild(booksDisplayAuthor)
-  
-    let booksDisplayEdition= document.createTextNode("Edition:"+book.edition);
+  let booksDisplayAuthor = document.createTextNode(
+    "Author:" + book.author + "\t"
+  );
+  booksDisplay.appendChild(booksDisplayAuthor);
 
-    booksDisplay.appendChild(booksDisplayEdition)
+  let booksDisplayEdition = document.createTextNode("Edition:" + book.edition);
 
+  booksDisplay.appendChild(booksDisplayEdition);
 
-    const button = document.createElement('button')
-    button.setAttribute('id',`button${_index}`);
-    booksDisplay.appendChild(button)
-    button.innerText = "Add to favorite"
-    
-    
+  const button = document.createElement("button");
+  button.setAttribute("id", `button${_index}`);
+  booksDisplay.appendChild(button);
+  button.innerText = "Add to favorite";
 
+  button.addEventListener("click", favoriteButton);
 
+  function favoriteButton() {
+    books[_index].favorite = true;
+    localStorage.setItem("myBooks", JSON.stringify([...books]));
+  }
 
-    button.onclick = function favoriteButton() {
-        
-        console.log("before",books)
-        books[_index].favorite = true;
-        
-        
-        localStorage.setItem("myBooks",JSON.stringify([...books]));
-        console.log("after",books)
-        
+  function compare( a, b ) {
+    if ( a.edition < b.edition){
+      return -1;
     }
-    // button.onclick = function handleButton() {
-    //     fav.push(book);
-    //     console.log(fav)
-    // }
-    
+    if ( a.edition > b.edition ){
+      return 1;
+    }
+    return 0;
+  }
+  
+  books.sort( compare );
+
 });
 
-// localStorage.setItem("myBooks",JSON.stringify(books));
